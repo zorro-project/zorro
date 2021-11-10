5cb801da8c83689320acbb56d6bb347f3e0646a4
Ted Suzman <ted@suzman.net>
2021-11-09 19:30:17 -0800

    wip starknetjs deploys

---
+++ starknet/deploy.js
@@ -0,0 +1,47 @@
+import fs from "fs";
+import { deployContract } from "starknet";
+
+function getCompiledContractString(path) {
+  return fs.readFileSync(path).toString("ascii");
+}
+
+const nymCompiledContractString = getCompiledContractString(
+  "./contracts/build/nym_compiled.json"
+);
+
+const accountCompiledContractString = getCompiledContractString(
+  "./contracts/build/OpenZepplin/Account_compiled.json"
+);
+
+async function deployAccount() {}
+
+async function deploy() {
+  const response = await deployContract(
+    nymCompiledContractString
+    /*
+    Contract.compileCalldata({
+      signer: randomAddress(),
+      guardian: "0",
+      L1_address: "0",
+    })*/
+  );
+  console.log("response", response);
+}
+
+/*
+const inputContract = compiledArgentAccount as unknown as CompiledContract;
+
+const response = await deployContract(
+  inputContract,
+  Contract.compileCalldata({
+    signer: randomAddress(),
+    guardian: '0',
+    L1_address: '0',
+  })
+);
+*/
+
+deploy().then(() => {
+  console.log("Done");
+  process.exit(0);
+});
