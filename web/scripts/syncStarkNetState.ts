e9dbb5482d42f43bb6811a0f774fdd290a238f61
Ted Suzman <ted@suzman.net>
2021-12-24 19:07:32 -0800

    script for syncStarkNetState

---
+++ web/scripts/syncStarkNetState.ts
@@ -0,0 +1,7 @@
+import syncStarkNetState from '$api/src/tasks/syncStarkNetState'
+
+export default async ({ args }) => {
+  console.log(':: Executing script with args ::')
+  console.log(args)
+  await syncStarkNetState()
+}
