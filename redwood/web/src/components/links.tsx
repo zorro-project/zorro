cc8886d647f3a0786b6bc36d1fde9c90fe632c87
Kyle Corbitt <kyle@corbt.com>
2022-01-11 12:30:18 +0100

    Forward the ref for RLinks
    This lets us use RLinks to create buttons that act as links to other pages within the Redwood app.

--- redwood/web/src/components/links.tsx
+++ redwood/web/src/components/links.tsx
@@ -7,10 +7,10 @@ import {
 } from '@chakra-ui/react'
 import {Link as RedwoodLink} from '@redwoodjs/router'
 
-export const RLink = ({href, ...props}: LinkProps) => (
+export const RLink = React.forwardRef(({href, ...props}: LinkProps, ref) => (
   // @ts-expect-error there's something incompatible with the `color` prop, not sure if it's a real issue or just a problem with the typescript definitions
-  <RedwoodLink to={href} {...props} />
-)
+  <RedwoodLink to={href} {...props} ref={ref} />
+))
 
 export const InternalLink = (props: LinkProps) => (
   <ChakraLink as={RLink} {...props} />
