01db4664e9fc9307337ab8facab00117e4b7b906
Ted Suzman <ted@suzman.net>
2022-01-25 17:48:36 -0800

    fix typo in type

--- redwood/api/src/services/cachedProfiles/cachedProfiles.ts
+++ redwood/api/src/services/cachedProfiles/cachedProfiles.ts
@@ -149,7 +149,7 @@ export const isVerified = (
     | 'challengeTimestamp'
     | 'superAdjudicationTimestamp'
     | 'didAdjudicatorVerifyProfile'
-    | 'didSuperAdjudicatorOverturnAdjudciator'
+    | 'didSuperAdjudicatorOverturnAdjudicator'
     | 'adjudicationTimestamp'
     | 'appealTimestamp'
     | 'submissionTimestamp'
