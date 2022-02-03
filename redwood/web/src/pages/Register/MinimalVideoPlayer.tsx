41c6398465306ad15948c227bc76e5491974dbb8
Kyle Corbitt <kyle@corbt.com>
2022-02-03 11:30:53 +0000

    Click video again to pause it
    Probably what users expect to be able to do.

--- redwood/web/src/pages/Register/MinimalVideoPlayer.tsx
+++ redwood/web/src/pages/Register/MinimalVideoPlayer.tsx
@@ -3,31 +3,36 @@ import ReactPlayer from 'react-player'
 import {Box, Icon, Center} from '@chakra-ui/react'
 import {FaPlay} from 'react-icons/fa'
 
-const MinimalVideoPlayer = (props: ReactPlayer['props']) => {
+const MinimalVideoPlayer = (
+  props: Omit<ReactPlayer['props'], 'width' | 'height' | 'playing' | 'onEnded'>
+) => {
   const [isPlaying, setIsPlaying] = useState(false)
-  const startPlaying = () => setIsPlaying(true)
   const stopPlaying = () => setIsPlaying(false)
+  const togglePlaying = () => setIsPlaying((value) => !value)
   return (
     <Box position="relative" h="100%" w="100%">
-      <ReactPlayer {...props} playing={isPlaying} onEnded={stopPlaying} />
+      <ReactPlayer
+        {...props}
+        width="100%"
+        height="100%"
+        playing={isPlaying}
+        onEnded={stopPlaying}
+      />
 
-      {!isPlaying && (
-        <Center
-          onClick={startPlaying}
-          sx={{
-            position: 'absolute',
-            backgroundColor: 'rgba(0,0,0,0.2)',
-            left: 0,
-            top: 0,
-            bottom: 0,
-            right: 0,
-            cursor: 'pointer',
-            _hover: {backgroundColor: 'rgba(0,0,0,0.3)'},
-          }}
-        >
-          <Icon as={FaPlay} w={20} h={20} color="white" opacity={0.65} />
-        </Center>
-      )}
+      <Center
+        onClick={togglePlaying}
+        position="absolute"
+        left="0"
+        right="0"
+        bottom="0"
+        top="0"
+        backgroundColor="rgba(0,0,0,0.2)"
+        _hover={{backgroundColor: 'rgba(0,0,0,0.3)'}}
+        cursor="pointer"
+        opacity={isPlaying ? 0 : 1}
+      >
+        <Icon as={FaPlay} w={20} h={20} color="white" opacity={0.65} />
+      </Center>
     </Box>
   )
 }
