b94f618dac6ef2975417a19751a88490a07eefa4
Ted Suzman <ted@suzman.net>
2022-02-08 11:30:37 -0800

    Hide your goerli eth from thieving bots

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,7 +1,7 @@
 import 'dotenv-defaults/config'
 
 import '@shardlabs/starknet-hardhat-plugin'
-import {HardhatUserConfig, task} from 'hardhat/config'
+import {HardhatUserConfig} from 'hardhat/config'
 import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
@@ -10,15 +10,12 @@ import 'solidity-coverage'
 
 import './tasks'
 
-const mnemonicPartOne: string | undefined = process.env.MNEMONIC_PART_ONE
-const mnemonicPartTwo: string | undefined = process.env.MNEMONIC_PART_TWO
-if (!mnemonicPartOne || !mnemonicPartTwo) {
+const ethereumMnemonic: string | undefined = process.env.ETHEREUM_MNEMONIC
+if (!ethereumMnemonic) {
   throw new Error(
-    'Please set MNEMONIC_PART_ONE and MNEMONIC_PART_TWO in a .env file'
+    'Please set ETHEREUM_MNEMONIC in .env file (and give it some goerli eth)'
   )
 }
-
-const mnemonic = `${mnemonicPartOne} ${mnemonicPartTwo}`
 
 const infuraApiKey: string | undefined = process.env.INFURA_API_KEY
 if (!infuraApiKey) {
@@ -32,7 +29,7 @@ const config: HardhatUserConfig = {
       url: `https://goerli.infura.io/v3/${infuraApiKey}`,
       accounts: {
         count: 4,
-        mnemonic,
+        mnemonic: ethereumMnemonic,
         path: "m/44'/60'/0'/0",
       },
     },
